c9d89949d580dd202266687a94243442ce6e4f13
dee797 <178827694+dee797@users.noreply.github.com>
2024-11-19 14:09:43 -0700

    Create calculator object and tests for it

--- src/index.js
+++ src/index.js
@@ -3,6 +3,39 @@ const capitalize = string => {
     return firstLetter += string.slice(1, string.length);
 }
 
+
+const reverseString = string => {
+    let newString = "";
+    for (let i = string.length - 1; i > - 1; i--) {
+        newString += string[i];
+    }
+    return newString;
+}
+
+
+const calculator = (function() {
+    const add = (num1, num2) => {
+        return Math.round((num1 + num2) * 100) / 100
+    }
+
+    const subtract = (num1, num2) => {
+        return Math.round((num1 - num2) * 100) / 100
+    } 
+
+    const multiply = (num1, num2) => {
+        return Math.round((num1 * num2) * 100) / 100
+    }
+
+    const divide = (num1, num2) => {
+        return Math.round((num1 / num2) * 100) / 100
+    }
+
+    return { add, subtract, multiply, divide }
+})();
+
+
 module.exports = {
     capitalize,
+    reverseString,
+    calculator,
 };
--- src/index.spec.js
+++ src/index.spec.js
@@ -1,6 +1,27 @@
-const { capitalize } = require("./index");
+const { capitalize, reverseString, calculator } = require("./index");
 
-test("works", () => {
+test("Capitalize first letter", () => {
     expect(capitalize("hello world")).toEqual("Hello world");
 });
 
+test("Reverse characters in a string", () => {
+    expect(reverseString("Hello World")).toEqual("dlroW olleH");
+});
+
+describe("calculator", () => {
+    test("Add two numbers", () => {
+        expect(calculator.add(33.222, 5.4)).toEqual(38.62);
+    });
+
+    test("Subtract two numbers", () => {
+        expect(calculator.subtract(23.571, 34.25)).toEqual(-10.68);
+    });
+
+    test("Multiply two numbers", () => {
+        expect(calculator.multiply(0, 3.33)).toEqual(0);
+    });
+
+    test("Divide two numbers", () => {
+        expect(calculator.divide(3, 2)).toEqual(1.50)
+    })
+})
